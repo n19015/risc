f173012c6cbd0e09e27ccbbd3bcb67e21b2122cc
fujino tatsunori <n19015@std.it-college.ac.jp>
2019-07-09 09:50:19 +0900

    がちゃがちゃ

--- usagi.py
+++ usagi.py
@@ -3,8 +3,10 @@ import random
 a = int(input("何回まわす？1~10 "))
 if a <= 10:
     for i in range(a):
+        #一文字ずつ取り出す関数
         word = random.sample(('さ', 'う', 'の', 'ぎ', 'く', 'に', 'れ', 'ば', 'な'), 9)
         print(''.join(word))
+        #でない人用
 elif a == 999:
     print("うさぎのにくばなれ")
 else:
